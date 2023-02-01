c995c0a4f36b1305c037971f91b8a0679a43a035
Mehran <raihan.mehran@hotmail.com>
2023-02-01 23:47:14 +0100

    Seed data code removed from Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -30,21 +30,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-try
-{
-    var context = services.GetRequiredService<DataContext>();
-    var userManager = services.GetRequiredService<UserManager<AppUser>>();
-    var roleMnaager = services.GetRequiredService<RoleManager<AppRole>>();
-    var mapper = services.GetRequiredService<IMapper>();
-    await context.Database.MigrateAsync();
-    await Seed.SeedUsers(userManager, roleMnaager, mapper);
-}
-catch (Exception ex)
-{
-    var logger = services.GetService<ILogger<Program>>();
-    logger.LogError(ex, "An error occured during migration");
-}
-
 app.Run();
