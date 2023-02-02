2bf42ff258035cc0a4ccf893cb3da17a73f45202
Mehran <raihan.mehran@hotmail.com>
2023-02-02 22:55:41 +0100

    AppUser Entity Added

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,6 +4,10 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public string IdNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
 
     }
 }
