ab526d2a1c74628dad2a2e4e41517dbcb3c6dade
Mehran <raihan.mehran@hotmail.com>
2023-02-03 17:24:51 +0100

    users retrieved in client

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,16 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<DataContext>(opt =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefautConnection"));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
