7ad70ae88c1eac75d56eceb820151e999d7f3c9f
Mehran <raihan.mehran@hotmail.com>
2023-02-04 16:00:58 +0100

    ssl certificate added to client app

--- API/Program.cs
+++ API/Program.cs
@@ -18,7 +18,7 @@ var app = builder.Build();
 app.UseCors(builder => builder
     .AllowAnyHeader()
     .AllowAnyMethod()
-    .WithOrigins("http://localhost:4200"));
+    .WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
