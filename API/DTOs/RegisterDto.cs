b5b51771950e78b3b4c8e6260d84dc2f5b008037
Mehran <raihan.mehran@hotmail.com>
2023-02-05 17:21:32 +0100

    Required DataAnnotation added to RegisterDto

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string IdNumber { get; set; }
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [Required]
         public string Country { get; set; }
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities
 {
     public class AppUser
