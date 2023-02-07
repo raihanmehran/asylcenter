0e0fd9675030b41b853bb8ebd9c0397ee1bd3059
Mehran <raihan.mehran@hotmail.com>
2023-02-07 19:18:03 +0100

    register component added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './admin/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
