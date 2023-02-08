b8b0bfcde151f923b4fa9ceb48f6d2a7f59f428b
Mehran <raihan.mehran@hotmail.com>
2023-02-08 16:39:03 +0100

    fields added in Register Component Template

--- client/src/app/admin/register/register.component.ts
+++ client/src/app/admin/register/register.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  model: any = {};
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
 }
