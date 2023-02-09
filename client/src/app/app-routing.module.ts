94dca179f9830dc0579705e8060e69789d5b4887
Mehran <raihan.mehran@hotmail.com>
2023-02-10 00:59:03 +0100

    components added to the app-routing-module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostComponent } from './admin/post/post.component';
+import { TasksComponent } from './admin/tasks/tasks.component';
+import { UserDetailComponent } from './admin/user-detail/user-detail.component';
+import { UsersListComponent } from './admin/users-list/users-list.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'admin', component: TasksComponent },
+  { path: 'users', component: UsersListComponent },
+  { path: 'users/:id', component: UserDetailComponent },
+  { path: 'post', component: PostComponent },
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
