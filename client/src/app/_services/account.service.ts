c062a431a2e0656f5dcb7be4e74dfe2148bab89e
Mehran <raihan.mehran@hotmail.com>
2023-02-09 23:05:32 +0100

    register user function added to accounService

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -25,6 +25,17 @@ export class AccountService {
     );
   }
 
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentuserSource.next(user);
+        }
+      })
+    );
+  }
+
   setCurrentUser(user: User) {
     this.currentuserSource.next(user);
   }
