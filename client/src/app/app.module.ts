7c7f1623017ec462f9d7690cf24d3ecf6e64bedf
Mehran <raihan.mehran@hotmail.com>
2023-02-11 00:32:23 +0100

    not-found and server-error components added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { PostComponent } from './post/post/post.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorComponent } from './errors/test-error.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +34,8 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
     PostDetailComponent,
     PostComponent,
     TestErrorComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
   ],
   imports: [
     BrowserModule,
