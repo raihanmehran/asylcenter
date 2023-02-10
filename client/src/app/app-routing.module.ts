2119dca83b1c5aafce7cb07fac0da7c26a306c50
Mehran <raihan.mehran@hotmail.com>
2023-02-10 01:08:48 +0100

    post components add to app-routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { TasksComponent } from './admin/tasks/tasks.component';
 import { UserDetailComponent } from './admin/user-detail/user-detail.component';
 import { UserListComponent } from './admin/user-list/user-list.component';
 import { HomeComponent } from './home/home.component';
+import { PostDetailComponent } from './post/post-detail/post-detail.component';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { PostComponent } from './post/post/post.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'admin', component: TasksComponent },
   { path: 'users', component: UserListComponent },
   { path: 'users/:id', component: UserDetailComponent },
+  { path: 'post', component: PostComponent },
+  { path: 'post/list', component: PostListComponent },
+  { path: 'post/list/:id', component: PostDetailComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
 
