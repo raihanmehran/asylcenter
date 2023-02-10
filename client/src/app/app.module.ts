d2c28b0c487a08edb25a67627f95028e1567c878
Mehran <raihan.mehran@hotmail.com>
2023-02-10 15:11:32 +0100

    shared module added in client for refactoring

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './admin/register/register.component';
 import { TasksComponent } from './admin/tasks/tasks.component';
@@ -16,7 +15,7 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { PostListComponent } from './post/post-list/post-list.component';
 import { PostDetailComponent } from './post/post-detail/post-detail.component';
 import { PostComponent } from './post/post/post.component';
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -37,10 +36,7 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right',
-    }),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
