b01e90aa68e481cb04ab05d111bf2d7c88928564
Mehran <raihan.mehran@hotmail.com>
2023-02-11 20:09:58 +0100

    User Repository added

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -15,6 +15,7 @@ namespace API.Extensions
             });
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
 
             return services;
         }
