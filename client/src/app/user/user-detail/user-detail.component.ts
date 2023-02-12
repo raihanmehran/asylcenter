2582fbc5e337f22df5b4541e98372c4fbe96ed7e
Mehran <raihan.mehran@hotmail.com>
2023-02-12 19:19:28 +0100

    user loaded in user-detail comp.

--- client/src/app/user/user-detail/user-detail.component.ts
+++ client/src/app/user/user-detail/user-detail.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/_models/user';
+import { UsersService } from 'src/app/_services/users.service';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+  styleUrls: ['./user-detail.component.css'],
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  user: User | undefined;
 
+  constructor(
+    private userService: UsersService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadUser();
+  }
+  loadUser() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.userService.getUser(username).subscribe({
+      next: (user) => (this.user = user),
+    });
+  }
 }
