c836017c1ac6c6f50a19a0938f5c5503056e52b5
Mehran <raihan.mehran@hotmail.com>
2023-02-13 23:23:18 +0100

    user retrieved in user-edit component

--- client/src/app/user/user-edit.component.ts
+++ client/src/app/user/user-edit.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { LoggedUser } from '../_models/loggedUser';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+import { UsersService } from '../_services/users.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
 })
-export class UserEditComponent {
+export class UserEditComponent implements OnInit {
+  user: User | undefined;
+  loggedUser: LoggedUser | null = null;
 
+  constructor(
+    private accountService: AccountService,
+    private userService: UsersService
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: (loggedUser) => (this.loggedUser = loggedUser),
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadUser();
+  }
+
+  loadUser() {
+    if (!this.loggedUser) return;
+
+    this.userService.getUser(this.loggedUser.username).subscribe({
+      next: (user) => (this.user = user),
+    });
+  }
 }
