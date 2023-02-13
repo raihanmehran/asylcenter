c2fd72daa7ec1f6ec0341e779108699a0f1b519b
Mehran <raihan.mehran@hotmail.com>
2023-02-13 16:03:05 +0100

    user photos displayed in phots tab in userDetail

--- client/src/app/user/user-detail/user-detail.component.ts
+++ client/src/app/user/user-detail/user-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { NgxGalleryAnimation } from '@kolkov/ngx-gallery';
 import { User } from 'src/app/_models/user';
 import { UsersService } from 'src/app/_services/users.service';
 
@@ -10,6 +12,8 @@ import { UsersService } from 'src/app/_services/users.service';
 })
 export class UserDetailComponent implements OnInit {
   user: User | undefined;
+  galleryOptions: NgxGalleryOptions[] = [];
+  galleryImages: NgxGalleryImage[] = [];
 
   constructor(
     private userService: UsersService,
@@ -18,12 +22,38 @@ export class UserDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadUser();
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false,
+      },
+    ];
+  }
+  getImages() {
+    if (!this.user) return [];
+    const imageUrls = [];
+
+    for (const photo of this.user.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+      });
+    }
+    return imageUrls;
   }
   loadUser() {
     const username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.userService.getUser(username).subscribe({
-      next: (user) => (this.user = user),
+      next: (user) => {
+        (this.user = user), (this.galleryImages = this.getImages());
+      },
     });
   }
 }
