46259c7b8d0f99f6883a7ae88af0f9912820e385
Mehran <raihan.mehran@hotmail.com>
2023-02-15 16:41:44 +0100

    services used to store state

--- client/src/app/_services/users.service.ts
+++ client/src/app/_services/users.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
@@ -8,18 +9,32 @@ import { User } from '../_models/user';
 })
 export class UsersService {
   baseUrl = environment.apiUrl;
+  users: User[] = [];
 
   constructor(private http: HttpClient) {}
 
   getUsers() {
-    return this.http.get<User[]>(this.baseUrl + 'users');
+    if (this.users.length > 0) return of(this.users);
+    return this.http.get<User[]>(this.baseUrl + 'users').pipe(
+      map((users) => {
+        this.users = users;
+        return users;
+      })
+    );
   }
 
   getUser(username: string) {
+    const user = this.users.find((u) => u.userName === username);
+    if (user) return of(user);
     return this.http.get<User>(this.baseUrl + 'users/' + username);
   }
 
   updateUser(user: User) {
-    return this.http.put(this.baseUrl + 'users', user);
+    return this.http.put(this.baseUrl + 'users', user).pipe(
+      map(() => {
+        const index = this.users.indexOf(user);
+        this.users[index] = { ...this.users[index], ...user };
+      })
+    );
   }
 }
