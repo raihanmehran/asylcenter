554bc66d9cadeda293e23b75b3d8529ab4765be4
Mehran <raihan.mehran@hotmail.com>
2023-02-15 17:43:00 +0100

    IPhoto interface implemented

---
+++ API/Services/PhotoService.cs
@@ -0,0 +1,33 @@
+using API.Helpers;
+using API.Interfaces;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+
+namespace API.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloudinary;
+        public PhotoService(IOptions<CloudinarySettings> config)
+        {
+            var acc = new Account
+            (
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(acc);
+        }
+        public Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeletionResult> DeletePhotoAsync(string publicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
