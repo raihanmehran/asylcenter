20d9da10476fbaac9a9df7c0439d91abaa62a435
Mehran <raihan.mehran@hotmail.com>
2023-02-15 13:10:23 +0100

    UserUpdateDto added

---
+++ API/DTOs/UserUpdateDto.cs
@@ -0,0 +1,9 @@
+namespace API.DTOs
+{
+    public class UserUpdateDto
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Language { get; set; }
+    }
+}
