d334a2ab5f1388c2d5d300aa40cf0bcf2c0131c8
Mehran <raihan.mehran@hotmail.com>
2023-02-16 18:06:33 +0100

    PhotoService implemented in API

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -19,6 +19,7 @@ namespace API.Extensions
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddScoped<IPhotoService, PhotoService>();
 
             return services;
         }
--- API/Services/PhotoService.cs
+++ API/Services/PhotoService.cs
@@ -20,14 +20,28 @@ namespace API.Services
 
             _cloudinary = new Cloudinary(acc);
         }
-        public Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
         {
-            throw new NotImplementedException();
+            var uploadResult = new ImageUploadResult();
+
+            if(file.Length > 0){
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face"),
+                    Folder = "asylcenter-net7"
+                };
+                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+            }
+
+            return uploadResult;
         }
 
-        public Task<DeletionResult> DeletePhotoAsync(string publicId)
+        public async Task<DeletionResult> DeletePhotoAsync(string publicId)
         {
-            throw new NotImplementedException();
+            var deleteParams = new DeletionParams(publicId);
+
+            return await _cloudinary.DestroyAsync(deleteParams);
         }
     }
 }
