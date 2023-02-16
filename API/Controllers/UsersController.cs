74b94d9208fc20b1725ae22a01338bf45f610087
Mehran <raihan.mehran@hotmail.com>
2023-02-16 19:08:02 +0100

    201 Created response generated in addPhoto endpoin

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -71,7 +71,13 @@ namespace API.Controllers
 
             user.Photos.Add(photo);
 
-            if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDto>(photo);
+            if (await _userRepository.SaveAllAsync())
+            {
+                return CreatedAtAction(
+                    nameof(GetUser),
+                    new { username = user.UserName },
+                    _mapper.Map<PhotoDto>(photo));
+            }
 
             return BadRequest("Problem happend in adding photo");
         }
