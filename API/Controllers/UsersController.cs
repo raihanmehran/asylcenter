21d1845aaf524c3ec4203020fc7f0f512752bb34
Mehran <raihan.mehran@hotmail.com>
2023-02-17 15:21:36 +0100

    Set Main Photo added in API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -81,5 +81,27 @@ namespace API.Controllers
 
             return BadRequest("Problem happend in adding photo");
         }
+
+        [HttpPut("set-main-photo/{photoId}")]
+        public async Task<ActionResult> SetMainPhoto(int photoId)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            if (user == null) return NotFound();
+
+            var photo = user.Photos.FirstOrDefault(p => p.Id == photoId);
+
+            if (photo == null) return NotFound();
+
+            if (photo.IsMain) return BadRequest("This is already your main photo");
+
+            var currentMain = user.Photos.FirstOrDefault(p => p.IsMain);
+            if (currentMain != null) currentMain.IsMain = false;
+            photo.IsMain = true;
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Problem setting the main photo");
+        }
     }
 }
