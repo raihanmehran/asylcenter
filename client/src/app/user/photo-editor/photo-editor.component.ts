770b2cf5b341b882e782293d366c40628df1c8d3
Mehran <raihan.mehran@hotmail.com>
2023-02-23 00:32:06 +0100

    user's first photo sat as profile picture

--- client/src/app/user/photo-editor/photo-editor.component.ts
+++ client/src/app/user/photo-editor/photo-editor.component.ts
@@ -84,6 +84,11 @@ export class PhotoEditorComponent implements OnInit {
       if (response) {
         const photo = JSON.parse(response);
         this.user?.photos.push(photo);
+        if (photo.isMain && this.loggedUser && this.user) {
+          this.loggedUser.photoUrl = photo.url;
+          this.user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.loggedUser);
+        }
       }
     };
   }
