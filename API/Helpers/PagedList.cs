6e990dcce0a5a3cb389ed25b047bd6ad2bbf1ddb
Mehran <raihan.mehran@hotmail.com>
2023-02-24 14:30:30 +0100

    CreateAsync method added in PagedList class

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Helpers
 {
     public class PagedList<T> : List<T>
@@ -21,5 +23,17 @@ namespace API.Helpers
         public int TotalPages { get; set; }
         public int PageSize { get; set; }
         public int TotalCount { get; set; }
+
+        public static async Task<PagedList<T>> CreateAsync(
+            IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
     }
 }
