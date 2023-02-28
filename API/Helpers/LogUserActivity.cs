fd041ff5c0de691599cf3acae087bfe8df773a8c
Mehran <raihan.mehran@hotmail.com>
2023-02-28 17:48:03 +0100

    Last Active action filter added in API

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,26 @@
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+
+            if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var username = resultContext.HttpContext.User.GetUsername();
+
+            var repo = resultContext.HttpContext.RequestServices
+                .GetRequiredService<IUserRepository>();
+
+            var user = await repo.GetUserByUsernameAsync(username: username);
+            user.LastActive = DateTime.Now;
+            await repo.SaveAllAsync();
+
+        }
+    }
+}
