14588ee386b32949b5c38071d7af533db3d04af5
Mehran <raihan.mehran@hotmail.com>
2023-03-02 19:37:00 +0100

    post-card component template configured

--- client/src/app/post/post-list/post-list.component.ts
+++ client/src/app/post/post-list/post-list.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
