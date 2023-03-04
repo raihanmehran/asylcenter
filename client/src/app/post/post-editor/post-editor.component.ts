4b432f707851332cd7c92dbe8c60fc44a888ef11
Mehran <raihan.mehran@hotmail.com>
2023-03-04 15:49:44 +0100

    post-editor component template configured

--- client/src/app/post/post-editor/post-editor.component.ts
+++ client/src/app/post/post-editor/post-editor.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-post-editor',
   templateUrl: './post-editor.component.html',
-  styleUrls: ['./post-editor.component.css']
+  styleUrls: ['./post-editor.component.css'],
 })
-export class PostEditorComponent {
+export class PostEditorComponent implements OnInit {
+  postForm: FormGroup = new FormGroup({});
+  validationErrors: string[] | undefined;
+
+  constructor(private fb: FormBuilder, private toastr: ToastrService) {}
+  ngOnInit(): void {}
+
+  addPost() {
+    console.log('Add Post method called');
+  }
 
 }
