aec60c47b4213d34b3d91cf3575c215e8f1bea08
Mehran <raihan.mehran@hotmail.com>
2023-03-04 16:43:33 +0100

    text-area reusable component configured

--- client/src/app/_forms/text-area/text-area.component.ts
+++ client/src/app/_forms/text-area/text-area.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-text-area',
   templateUrl: './text-area.component.html',
-  styleUrls: ['./text-area.component.css']
+  styleUrls: ['./text-area.component.css'],
 })
-export class TextAreaComponent {
+export class TextAreaComponent implements ControlValueAccessor {
+  @Input() label = '';
+  @Input() id = '';
 
+  constructor(@Self() public ngControl: NgControl) {}
+
+  writeValue(obj: any): void {}
+  registerOnChange(fn: any): void {}
+  registerOnTouched(fn: any): void {}
+
+  get control(): FormControl {
+    return this.ngControl.control as FormControl;
+  }
 }
