964cda0f6ae8b1c466c56fefb2c69013c02678e8
Mehran <raihan.mehran@hotmail.com>
2023-03-10 14:06:05 +0100

    Event Entity extended

--- API/DTOs/EventDto.cs
+++ API/DTOs/EventDto.cs
@@ -1,17 +1,16 @@
-namespace API.Entities
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
 {
-    public class Event
+    public class EventDto
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Content { get; set; }
         public DateOnly Date { get; set; }
         public TimeOnly Time { get; set; }
         public string PhotoUrl { get; set; }
         public string PublicId { get; set; }
-        public DateTime Created { get; set; } = DateTime.Now;
-        public bool IsDeleted { get; set; } = false;
-        public int AddedBy { get; set; }
-        public List<EventFeedback> EventFeedback { get; set; } = new();
     }
 }
---
+++ API/Data/Migrations/20230310130402_EventEntityExtended.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventEntityExtended : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Events",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Events",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Events");
+        }
+    }
+}
