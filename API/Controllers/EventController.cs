75d0dea6e3e04cda45618bfc771ba797edf85ca9
Mehran <raihan.mehran@hotmail.com>
2023-03-12 03:23:21 +0100

    Created date in Event is fixed

--- API/Controllers/EventController.cs
+++ API/Controllers/EventController.cs
@@ -34,6 +34,7 @@ namespace API.Controllers
 
             var events = _mapper.Map<Event>(eventDto);
             events.AddedBy = user.Id;
+            events.Created = DateTime.Now;
 
             if (photo is not null)
             {
