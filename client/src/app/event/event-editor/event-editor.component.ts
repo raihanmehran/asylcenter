838456ca434942b5a242d60d91e49b8ca7f6602a
Mehran <raihan.mehran@hotmail.com>
2023-03-12 23:45:30 +0100

    event-editor template expand

--- client/src/app/event/event-editor/event-editor.component.ts
+++ client/src/app/event/event-editor/event-editor.component.ts
@@ -1,6 +1,12 @@
 import { Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { EventService } from 'src/app/_services/event.service';
 
@@ -11,6 +17,8 @@ import { EventService } from 'src/app/_services/event.service';
 })
 export class EventEditorComponent implements OnInit {
   eventForm: FormGroup = new FormGroup({});
+  imageUrl: string = '';
+  image: File | undefined;
 
   constructor(
     private fb: FormBuilder,
@@ -26,9 +34,36 @@ export class EventEditorComponent implements OnInit {
     this.eventForm = this.fb.group({
       title: ['', Validators.required],
       content: ['', Validators.maxLength(5000)],
-      date: [''],
+      date: ['', Validators.required],
       time: [''],
       location: [''],
     });
+  }
+  onFileSelected(event: any) {
+    this.imageUrl = '';
+    const file: File = event.target.files[0];
+
+    if (!file.type.startsWith('image/')) {
+      this.toastr.warning('Invalid file type selected!');
+    } else {
+      if (file) {
+        this.image = file;
+
+        console.log(file);
+
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          const fileContents = e.target.result;
+          this.imageUrl = fileContents;
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+
+  resetForm() {
+    this.initializeForm();
+    this.imageUrl = '';
+    this.image = undefined;
   }
 }
