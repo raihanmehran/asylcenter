2ad79053541a8c419afd1443387d6368d377356d
Mehran <raihan.mehran@hotmail.com>
2023-03-12 15:17:33 +0100

    event form initialized

--- client/src/app/event/event-editor/event-editor.component.ts
+++ client/src/app/event/event-editor/event-editor.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { EventService } from 'src/app/_services/event.service';
 
 @Component({
   selector: 'app-event-editor',
   templateUrl: './event-editor.component.html',
-  styleUrls: ['./event-editor.component.css']
+  styleUrls: ['./event-editor.component.css'],
 })
-export class EventEditorComponent {
+export class EventEditorComponent implements OnInit {
+  eventForm: FormGroup = new FormGroup({});
 
+  constructor(
+    private fb: FormBuilder,
+    private toastr: ToastrService,
+    private eventService: EventService
+  ) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.eventForm = this.fb.group({
+      title: ['', Validators.required],
+      content: ['', Validators.maxLength(5000)],
+      date: [''],
+      time: [''],
+      location: [''],
+    });
+  }
 }
