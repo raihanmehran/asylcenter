b21e3206074e3558e373883ab09c0e22f6a8c5eb
Mehran <raihan.mehran@hotmail.com>
2023-03-12 02:58:07 +0100

    event-card configured

--- client/src/app/event/event-card/event-card.component.ts
+++ client/src/app/event/event-card/event-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Time } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Events } from 'src/app/_models/events';
 
 @Component({
   selector: 'app-event-card',
   templateUrl: './event-card.component.html',
-  styleUrls: ['./event-card.component.css']
+  styleUrls: ['./event-card.component.css'],
 })
-export class EventCardComponent {
+export class EventCardComponent implements OnInit {
+  @Input() event: Events | undefined;
 
+  constructor() {}
+  ngOnInit(): void {}
 }
