a7cef8ef725c176ae53ef455fd661cac3fd02e23
Mehran <raihan.mehran@hotmail.com>
2023-03-12 02:19:51 +0100

    events fetched in event-list component

--- client/src/app/event/event-list/event-list.component.ts
+++ client/src/app/event/event-list/event-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Events } from 'src/app/_models/events';
+import { EventService } from 'src/app/_services/event.service';
 
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.css']
+  styleUrls: ['./event-list.component.css'],
 })
-export class EventListComponent {
+export class EventListComponent implements OnInit {
+  events: Events[] = [];
 
+  constructor(private eventService: EventService) {}
+  ngOnInit(): void {
+    this.getEvents();
+  }
+
+  getEvents() {
+    this.eventService
+      .getEvents()
+      .pipe()
+      .subscribe({
+        next: (response) => {
+          if (response) {
+            this.events = response;
+            console.log(this.events);
+          }
+        },
+      });
+  }
 }
