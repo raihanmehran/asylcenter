0c3fd714d6e0f2fc2564a1c0db945738a3fa541e
Mehran <raihan.mehran@hotmail.com>
2023-03-13 12:14:37 +0100

    events fetched and and displayed in search-event

--- client/src/app/event/event-search-card/event-search-card.component.ts
+++ client/src/app/event/event-search-card/event-search-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Events } from 'src/app/_models/events';
 
 @Component({
   selector: 'app-event-search-card',
   templateUrl: './event-search-card.component.html',
-  styleUrls: ['./event-search-card.component.css']
+  styleUrls: ['./event-search-card.component.css'],
 })
-export class EventSearchCardComponent {
+export class EventSearchCardComponent implements OnInit {
+  @Input() event: Events | undefined;
 
+  constructor() {}
+  ngOnInit(): void {}
 }
--- client/src/app/event/event-search/event-search.component.ts
+++ client/src/app/event/event-search/event-search.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Events } from 'src/app/_models/events';
+import { EventService } from 'src/app/_services/event.service';
 
 @Component({
   selector: 'app-event-search',
   templateUrl: './event-search.component.html',
   styleUrls: ['./event-search.component.css'],
 })
-export class EventSearchComponent {
+export class EventSearchComponent implements OnInit {
   events: Events[] | undefined;
+
+  constructor(
+    private eventService: EventService,
+    private toastr: ToastrService
+  ) {}
+  ngOnInit(): void {
+    this.getEvents();
+  }
+
+  getEvents() {
+    this.eventService
+      .getEvents()
+      .pipe()
+      .subscribe({
+        next: (response) => {
+          if (response) {
+            this.events = response;
+          }
+        },
+        error: (error) => this.toastr.error(error.error),
+      });
+  }
 }
