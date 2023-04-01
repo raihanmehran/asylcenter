a97e902f2976d80da77fb6ec87bc8d1313921b3e
Mehran <raihan.mehran@hotmail.com>
2023-04-01 23:40:28 +0200

    createHubConnection method added in presenceServic

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HubConnection } from '@microsoft/signalr';
+import { HubConnectionBuilder } from '@microsoft/signalr/dist/esm/HubConnectionBuilder';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from 'src/environments/environment';
+import { LoggedUser } from '../_models/loggedUser';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PresenceService {
+  hubUrl = environment.hubUrl;
+  private hubConnection?: HubConnection;
 
-  constructor() { }
+  constructor(private toastr: ToastrService) {}
+
+  createHubConnection(user: LoggedUser) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + 'presence', {
+        accessTokenFactory: () => user.token,
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection.start().catch((error) => console.log(error));
+
+    this.hubConnection.on('UserIsOnline', (username) => {
+      this.toastr.info(username + ' has connected');
+    });
+
+    this.hubConnection.on('UserIsOffline', (username) => {
+      this.toastr.warning(username + ' has disconnected');
+    });
+  }
 }
