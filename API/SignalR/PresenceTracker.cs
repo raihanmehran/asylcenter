a2af4fa268aa5d77bc7f6ec5e807d6ce12ac9e81
Mehran <raihan.mehran@hotmail.com>
2023-04-03 23:56:05 +0200

    UserConnected method added PresenceTracker

--- API/SignalR/PresenceTracker.cs
+++ API/SignalR/PresenceTracker.cs
@@ -2,6 +2,23 @@ namespace API.SignalR
 {
     public class PresenceTracker
     {
-       
+        private static readonly Dictionary<string, List<string>> OnlineUsers = new Dictionary<string, List<string>>();
+
+        public Task UserConnected(string username, string connectionId)
+        {
+            lock (OnlineUsers)
+            {
+                if (OnlineUsers.ContainsKey(username))
+                {
+                    OnlineUsers[username].Add(connectionId);
+                }
+                else
+                {
+                    OnlineUsers.Add(username, new List<string> { connectionId });
+                }
+            }
+
+            return Task.CompletedTask;
+        }
     }
 }
