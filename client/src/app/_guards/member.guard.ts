a5552e5a92d73f4f7340ebbbc26f743dd3af1b09
Mehran <raihan.mehran@hotmail.com>
2023-04-07 13:48:02 +0200

    member-guard added

---
+++ client/src/app/_guards/member.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { map, Observable } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberGuard implements CanActivate {
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
+
+  canActivate(): Observable<boolean> {
+    return this.accountService.currentUser$.pipe(
+      map((user) => {
+        if (!user) return false;
+        if (user.roles.includes('Member')) {
+          return true;
+        } else {
+          this.toastr.error('You cannot enter this area');
+          return false;
+        }
+      })
+    );
+  }
+}
