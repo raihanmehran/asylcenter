ab56d17a81480a1b54a8ed1838b28477494218ec
Mehran <raihan.mehran@hotmail.com>
2023-04-09 00:43:54 +0200

    navbar component redesigned

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -38,6 +38,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.router.navigateByUrl('/');
+    this.router.navigateByUrl('/home');
   }
 }
