95dc2c453544b632d0df5f5dcad66b0155d8dd69
Mehran <raihan.mehran@hotmail.com>
2023-04-15 00:31:45 +0200

    admin-panel component refactored

--- client/src/app/admin/charts/pie-chart/pie-chart.component.ts
+++ client/src/app/admin/charts/pie-chart/pie-chart.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { AccountService } from 'src/app/_services/account.service';
+import { DashboardService } from 'src/app/_services/dashboard.service';
+import { PresenceService } from 'src/app/_services/presence.service';
 
 @Component({
   selector: 'app-pie-chart',
@@ -8,11 +11,66 @@ import { BaseChartDirective } from 'ng2-charts';
   styleUrls: ['./pie-chart.component.css'],
 })
 export class PieChartComponent implements OnInit {
+  membersCount: number = 0;
+  moderatorsCount: number = 0;
+  adminsCount: number = 0;
+  OnlineUsers: number = 0;
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-  constructor() {}
-  ngOnInit(): void {
+  constructor(
+    private dashboardService: DashboardService,
+    private presenceService: PresenceService
+  ) {
+    this.getChartData();
+  }
+  ngOnInit(): void {}
+
+  getChartData() {
+    this.dashboardService.memberUsers$.subscribe({
+      next: (users) => {
+        this.membersCount = 0;
+        for (let user of users) {
+          this.membersCount += user.count;
+        }
+        this.setChartData();
+      },
+    });
+
+    this.dashboardService.moderatorUsers$.subscribe({
+      next: (users) => {
+        this.moderatorsCount = 0;
+        for (let user of users) {
+          this.moderatorsCount += user.count;
+        }
+        this.setChartData();
+      },
+    });
+
+    this.dashboardService.adminUsers$.subscribe({
+      next: (users) => {
+        this.adminsCount = 0;
+        for (let user of users) {
+          this.adminsCount += user.count;
+        }
+        this.setChartData();
+      },
+    });
+
+    this.presenceService.onlineUsers$.subscribe({
+      next: (users) => {
+        this.OnlineUsers = users.length;
+        this.setChartData();
+      },
+    });
+  }
+
+  setChartData() {
     this.pieChartData.labels = ['Online', 'Members', 'Moderators', 'Admins'];
-    this.pieChartData.datasets[0].data = [30, 60, 10, 30];
+    this.pieChartData.datasets[0].data = [
+      this.OnlineUsers,
+      this.membersCount,
+      this.moderatorsCount,
+      this.adminsCount,
+    ];
     this.chart?.update();
   }
 
@@ -26,6 +84,7 @@ export class PieChartComponent implements OnInit {
       },
     },
   };
+
   public pieChartData: ChartData<'pie', number[], string | string[]> = {
     labels: [],
     datasets: [
