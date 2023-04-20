fbba62b9e938d095053a8bf6c67b063fb330eeaa
Mehran <raihan.mehran@hotmail.com>
2023-04-20 23:21:20 +0200

    Fly.Io connection string removed from program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -34,30 +34,7 @@ if (builder.Environment.IsDevelopment())
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
 else if (builder.Environment.IsProduction())
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
-else
-{
-    // Use connection string provided at runtime by FlyIO.
-    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-    System.Console.WriteLine("DATABASE URL: " + connUrl);
-    if (string.IsNullOrEmpty(connUrl))
-    {
-        throw new Exception("The DATABASE_URL environment variable is not set.");
-    }
-
-    // Parse connection URL to connection string for Npgsql
-    connUrl = connUrl.Replace("postgres://", string.Empty);
-    var pgUserPass = connUrl.Split("@")[0];
-    var pgHostPortDb = connUrl.Split("@")[1];
-    var pgHostPort = pgHostPortDb.Split("/")[0];
-    var pgDb = pgHostPortDb.Split("/")[1];
-    var pgUser = pgUserPass.Split(":")[0];
-    var pgPass = pgUserPass.Split(":")[1];
-    var pgHost = pgHostPort.Split(":")[0];
-    var pgPort = pgHostPort.Split(":")[1];
-
-    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
-}
-System.Console.WriteLine("CONNECTION STRING: " + connString);
+    
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseNpgsql(connString);
